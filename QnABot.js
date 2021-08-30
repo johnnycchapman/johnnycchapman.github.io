23fc863145711f6d97823fd23cebd03990161b41
Johnny Chapman <chapmancjohnny@gmail.com>
2021-08-30 15:20:01 -0400

    delete maximize function

--- QnABot.js
+++ QnABot.js
@@ -9,10 +9,6 @@ function openChat() {
     document.getElementById('iframe1').style.height = "500px"
   }
   
-  function maximizeChat(){
-    document.getElementById('iframe1').style.height = "700px"
-  }
-  
   function closeChat() {
     document.getElementById("myChat").style.display = "none";
     document.getElementById("chat-btn").style.display = "none";
